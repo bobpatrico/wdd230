a6569466501e3c0ff230a5d5b9d16688f249b791
bobpatrico <owe20015@byui.edu>
2023-01-07 11:39:28 +0300

    comit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+window.onload = function date() {
+
+    //Get Year
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+    //Get last modified date
+    document.getElementById("update").innerHTML = document.lastModified;
+
+}
